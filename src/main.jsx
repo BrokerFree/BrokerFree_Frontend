35445dd6305f5ff9a7a2ae9eb9e1085db0d6830d
BrokerFree <help.brokerfree@gmail.com>
2025-11-19 14:41:02 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,14 +7,16 @@ import { Auth0Provider } from "@auth0/auth0-react";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Auth0Provider
-      domain="brokerfree.us.auth0.com"
-      // domain="dev-ajf4yrl342shon86.us.auth0.com"
-      clientId="0ADXwFEXJyW9JBWfN4JbkNJdP2MjBoRs"
-      // clientId="1OS4NYfaa4rFi2VoTpj1sl9S7Jqd3PY1"
-      audience="https://brokerfree-api"
-      scope="openid profile email"
-    >
-      <App />
-    </Auth0Provider>
+  domain="brokerfree.us.auth0.com"
+  clientId="0ADXwFEXJyW9JBWfN4JbkNJdP2MjBoRs"
+  authorizationParams={{
+    redirect_uri: window.location.origin,
+    audience: "https://brokerfree-api",
+    scope: "openid profile email"
+  }}
+>
+  <App />
+</Auth0Provider>
+
   </React.StrictMode>
 );
